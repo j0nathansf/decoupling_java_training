0a67dd7f59a86e766ebf9c2a8d537b2023fdd94d
Jonathan <jonathan.ren@protonmail.com>
2022-03-07 19:39:52 +0100

    ready

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,8 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new ContextualLogger("ConsoleLogger", new ConsoleLogger());
+        Logger log1 = new ConsoleLogger();
+        Logger log2 = new FileLogger("src/log.txt");
+        return new CompositeLogger(log1, log2);
     }
 }
